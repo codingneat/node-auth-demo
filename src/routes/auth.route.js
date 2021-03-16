d5702634612cf0c32a6f8d920f18b0177cea99df
Daniel Vergara <daniel.vergara.echeverri@gmail.com>
2021-03-16 17:52:40 -0500

    Clean Github Session Branch

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,12 +1,8 @@
 import { Router } from "express";
-import { signin, signup } from "../services/auth.service.js";
-import { authLocal } from "../strategies/local.strategy.js";
 import { authGithub, authGithubError } from "../strategies/github.strategy.js";
 
 const router = Router();
 
-router.post("/", authLocal, signin);
-router.post("/signup", signup);
 router.get("/github", authGithub);
 router.get("/github/callback", authGithubError, (req, res) => res.redirect('/'));
 
